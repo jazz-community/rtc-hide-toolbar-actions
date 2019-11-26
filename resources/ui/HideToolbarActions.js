86a9b20583007deeb32de443e33a4c48d9fbba4f
MartinBenninger <martin.benninger@outlook.com>
2019-11-26 14:33:08 +0100

    Add a function to remove buttons by their title attribute value

--- resources/ui/HideToolbarActions.js
+++ resources/ui/HideToolbarActions.js
@@ -1,8 +1,10 @@
 define([
   "dojo/_base/declare",
+  "dojo/dom-construct",
+  "dojo/query",
   "com.ibm.team.workitem.web.ui2.internal.action.AbstractAction",
   "dojo/domReady!"
-], function(declare) {
+], function(declare, domConstruct, query) {
   // Note that all of the above imports of ibm classes will log an error to the console but the classes are still loaded.
   // Using dojo.require doesn't log an error but also doesn't require the module when using AMD syntax.
 
@@ -24,6 +26,17 @@ define([
       // Always disable the action
       isEnabled: function(params) {
         return false;
+      },
+
+      _removeButtonByTitle: function(buttonTitle) {
+        query(
+          ".com-ibm-team-workItem-workItemEditorHeader .jazz-ui-Toolbar " +
+            ".jazz-ui-toolbar-Button .button[title='" +
+            buttonTitle +
+            "']"
+        ).forEach(function(node) {
+          domConstruct.destroy(node.parentElement);
+        });
       }
     }
   );
