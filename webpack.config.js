b8e31eb634117fa0129266c74e29f1178b0dc057
Martin Benninger <martin.benninger@outlook.com>
2019-12-12 00:34:38 +0100

    Only set the output variables when running from GitHub Actions

--- webpack.config.js
+++ webpack.config.js
@@ -52,9 +52,11 @@ module.exports = env => {
     ]
   };
 
-  // Set the output file name for use in GitHub Actions
-  core.setOutput("outputFile", `${projectId}_${version}.zip`);
-  core.setOutput("commitSha", process.env["GITHUB_SHA"]);
+  if (process.env["GITHUB_ACTIONS"]) {
+    // Set the output file name for use in GitHub Actions
+    core.setOutput("outputFile", `${projectId}_${version}.zip`);
+    core.setOutput("commitSha", process.env["GITHUB_SHA"]);
+  }
 
   return config;
 };
